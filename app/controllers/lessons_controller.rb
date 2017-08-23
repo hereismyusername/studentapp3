8f68bf1498fed44d6e8fd323fd9738658e36b030
hereismyusername <my.e.address123@gmail.com>
2017-08-23 16:22:16 +0000

    add lesson permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
+
   def show
   end
 
 
   private
+
+  def require_authorized_for_current_lesson
+    #if current_lesson.section.course != current_user.enrolled_in? (does not work)
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      #return render plain: 'Unauthorized', status: :unauthorized (before updates)
+      redirect_to course_path(current_lesson.section.course), alert: 'Please enroll in this course in order to view the lessons'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
